98f5db224393fe0b2d8b493c58f0ba7b98b76af2
actuallyrizzn <guesswho@rizzn.com>
2025-12-23 19:54:20 -0600

    chore: remove deprecated JavaScript files and configuration; clean up project structure by deleting unused scripts and configuration files

--- tmp/@walletconnect-core/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Core as WalletConnectCore } from "./core.js";
-
-export * from "./constants/index.js";
-export * from "./controllers/index.js";
-
-export const Core = WalletConnectCore;
-export default WalletConnectCore;
